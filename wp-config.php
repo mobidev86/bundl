da81e0ad88042584463c8955cd42913cdac795a9
Manish Patel <manish@acquaintsoft.com>
2019-04-29 05:24:04 +0000

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'bundl' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'root@2019' );
+define( 'DB_PASSWORD', 'aPZ@f9&d' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
